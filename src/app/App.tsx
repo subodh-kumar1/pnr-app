f0c44e9860415bae640ba3689ba53fdb9c939e27
Subodh Kumar <subodh.subodhkumar1@gmail.com>
2022-09-30 21:18:26 +0530

    deprecate prev branch stuffs

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,9 @@
-import Box from "@mui/material/Box";
 import React from "react";
-import {airCodes} from "../data/airCodes";
 
 const App = () => {
   return (
     <React.Fragment>
-      {airCodes.map((ele, index) => {
-        return (
-          <Box key={index}>
-            {ele.iata_code} {ele.name} {ele.icao_code}
-          </Box>
-        );
-      })}
+      Hello React
     </React.Fragment>
   );
 };
